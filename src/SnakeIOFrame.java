c6fdfbfff2cf0412ed644adbd295f72c40d0a6c4
fredrick-kamau1 <70659811+fredrick-kamau1@users.noreply.github.com>
2022-07-03 18:27:17 -0500

    Updates

--- src/SnakeIOFrame.java
+++ src/SnakeIOFrame.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class SnakeIOFrame {
+import javax.swing.JFrame;
+
+public class SnakeIOFrame extends JFrame {
+
+  SnakeIOFrame(){
+    this.add(new SnakeIOScreen());
+    this.setTitle("SnakeIO");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setResizable(false);
+    this.pack();
+    this.setVisible(true);
+    this.setLocationRelativeTo(null);
+  }
 }
--- src/SnakeIOMain.java
+++ src/SnakeIOMain.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class SnakeIOMain {
+public class SnakeIOMain {
+
+  public static void main(String[] args) {
+    new SnakeIOFrame();
+  }
 }
